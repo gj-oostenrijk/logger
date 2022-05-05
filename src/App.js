e33eb37c4c12a074aab0fbc395e2026b108aad32
gj-oostenrijk <gj-oostenrijk@users.noreply.github.com>
2022-05-05 16:32:57 +0200

    fix links for real

--- src/App.js
+++ src/App.js
@@ -11,18 +11,18 @@ function App() {
     <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="/">Logger App</Navbar.Brand>
+          <Navbar.Brand as={NavLink} to="/">Logger App</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link as={NavLink} to="/">Home</Nav.Link>
               <Nav.Link as={NavLink} to="/about">About</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item><Nav.Link as={NavLink} to="/stool">Stool logger</Nav.Link></NavDropdown.Item>
-                <NavDropdown.Item><Nav.Link as={NavLink} to="/users">User Management</Nav.Link></NavDropdown.Item>
-                <NavDropdown.Item><Nav.Link as={NavLink} to="/action/3.3">Something</Nav.Link></NavDropdown.Item>
+                <NavDropdown.Item as={NavLink} to="/stool">Stool logger</NavDropdown.Item>
+                <NavDropdown.Item as={NavLink} to="/users">User Management</NavDropdown.Item>
+                <NavDropdown.Item as={NavLink} to="/action/3.3">Something</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item><Nav.Link as={NavLink} to="/action/3.4">Separated link</Nav.Link></NavDropdown.Item>
+                <NavDropdown.Item as={NavLink} to="/action/3.4">Separated link</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
